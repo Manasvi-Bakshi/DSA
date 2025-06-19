c5bb1da342b7e9b419078a87293bfdb093f9dfb2
Newbie <83895867+Manasvi-Bakshi@users.noreply.github.com>
2025-06-19 11:58:53 +0530

    Time: 110 ms (24.19%) | Memory: 28.4 MB (90.38%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        water = 0
+        h1 = 0
+        h2 = len(height)-1
+        while h1 < h2:
+            breadth = min(height[h1],height[h2])
+            length = h2-h1
+            area = length * breadth
+            water = max(water, area)
+            if height[h1] < height[h2]:
+                h1 += 1
+            else:
+                h2 -= 1
+        return water
